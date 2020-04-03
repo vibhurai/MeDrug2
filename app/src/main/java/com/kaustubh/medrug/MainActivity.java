23de703601d90baa7e7f05b560ee64df4d4b162e
Kaustub <kr845@snu.edu.in>
2020-04-04 02:23:35 +0530

    THIS IS KAUSTUBH

---
+++ app/src/main/java/com/kaustubh/medrug/MainActivity.java
@@ -0,0 +1,30 @@
+package com.kaustubh.medrug;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+//    https://www.youtube.com/watch?v=I-I67MNRQZM
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+            Button button = (Button) findViewById(R.id.button2);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    openActivity2();
+                }
+            });
+        }
+
+        public void openActivity2() {
+            Intent intent = new Intent(this, menu.class);
+            startActivity(intent);
+        }
+}
---
+++ app/src/main/java/com/kaustubh/medrug/menu.java
@@ -0,0 +1,28 @@
+package com.kaustubh.medrug;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        Button b1 = findViewById(R.id.emergency_button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_DIAL);
+                i.setData(Uri.parse("tel:9428394228"));
+                startActivity(i);
+            }
+        });
+
+    }
+}
